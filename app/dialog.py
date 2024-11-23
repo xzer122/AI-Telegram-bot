4f0618922e896600817183b755f307e4edaedf4e
Dmitrii Valiaev <65446812+xzer122@users.noreply.github.com>
2024-11-23 13:14:58 +0700

    changed Const to Format

--- app/dialog.py
+++ app/dialog.py
@@ -1,19 +1,20 @@
 from aiogram_dialog import Dialog, Window
 from aiogram_dialog.widgets.input import MessageInput
 from aiogram_dialog.widgets.kbd import Next
-from aiogram_dialog.widgets.text import Const
+from aiogram_dialog.widgets.text import Const, Format
 
 from app import texts
 from states import AcquaintanceSG
+from handlers import get_name, get_date_of_birth
 
 acquaintance = Dialog(
     Window(Const(texts.greeting),
            Next(Const("Начать знакомство")),
            state=AcquaintanceSG.greeting),
-    Window(Const(texts.name),
+    Window(Format("{ai_response_name}"),
            MessageInput(get_name),
            state=AcquaintanceSG.name),
-    Window(Const(texts.age),
+    Window(Format("{ai_response_name}"),
            MessageInput(get_date_of_birth),
            state=AcquaintanceSG.date_of_birth)
 )
